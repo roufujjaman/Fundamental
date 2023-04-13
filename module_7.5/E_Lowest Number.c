66f32b30886943e3d3aaf6dcb0fec421e0a67d3f
Roufujjaman <rrhtcode@gmail.com>
2023-04-13 08:31:01 +0600

    13.4.23 M

---
+++ module_7.5/E_Lowest Number.c
@@ -0,0 +1,24 @@
+//          Lowest Number
+#include <stdio.h>
+#include<limits.h>
+int main()
+{
+    int count, position;
+    int min = INT_MAX;
+    scanf("%d", &count);
+    int arr[count];
+    for (int i = 0; i < count; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0 - 1 ; i < count; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+            position = i+1;
+        }
+    }
+    printf("%d %d", min, position);
+    return 0;
+}
