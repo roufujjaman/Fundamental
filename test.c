1f60a55594496e60f3cc3e16140bc2ac64782d43
Roufujjaman <rrhtcode@gmail.com>
2023-04-17 21:51:46 +0600

    17.4.23 N

--- test.c
+++ test.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
 int main() {
-    int count;
-    scanf("%d", &count);
-    int arr[count + 1];
-    if (count == 0)
+    char name[18];
+    while (!"\0")   
     {
-        return 0;
+        scanf("%c", name);
     }
-    for (int i = 0; i < count; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    int pos, val;
-    scanf("%d %d", &pos, &val);
-    // count = 5, but for array its 6th position 
-    for (int i = count; i >= pos + 1; i--)
-    {
-        arr[i] = arr[i -1];
-    }
-    arr[pos] = val;
+    
+    printf("%s", name);
 
-    for (int i = 0; i <= count; i++)
-    {
-        printf("[%d]", arr[i]);
-    }
     return 0;
 }
