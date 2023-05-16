185242e8dad5ee564e7ff57ea9542709b2dee05d
Roufujjaman <rrhtcode@gmail.com>
2023-05-16 19:39:16 +0600

    16 5 23 E

--- test.c
+++ test.c
@@ -3,27 +3,20 @@ int main()
 {
     int num_value;
     scanf("%d", &num_value);
-    char arr[num_value + 1];
-    // for (int i = 0; i < num_value; i++)
-    // {
-    //     scanf("%c", &arr[i]);
-    // }
-    scanf("%s", arr);
-    int arr_alphabets[26] = {0};
+    char arr_alphabet[26] = {0};
     for (int i = 0; i < num_value; i++)
     {
-        arr_alphabets[arr[i] - 'a']++;
+        char current_char;
+        scanf(" %c", &current_char);
+        arr_alphabet[current_char - 'a']++;
     }
-    int i = 0;
-    while (i < 26)
+    for (int i = 0; i < 26; i++)
     {
-        // printf("%d", i);
-        if(arr_alphabets[i] != 0)
+        while (arr_alphabet[i] != 0)
         {
             printf("%c", i + 'a');
+            arr_alphabet[i]--;
         }
-        i++;
     }
     return 0;
 }
-
