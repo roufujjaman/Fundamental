01ba1c0c1c4b3b725d7b210c8bfaf1e818ad8e4c
Roufujjaman <rrhtcode@gmail.com>
2023-05-23 01:00:57 +0600

    23 5 23

--- sketch.cpp
+++ sketch.cpp
@@ -2,17 +2,29 @@
 using namespace std;
 int main()
 {
-    int a, b, c;
-    cin >> count;
-    int n;
-    int max = INT_MIN;
-    int min = INT_MAX;
-    for (int i = 0; i < count; i++)
+    int * arr = new int[5];
+    int * arr_copy = new int[7];
+    for (int i = 0; i < 5; i++)
     {
-        cin >> n;
-        if (n > max) max = n;
-        if (n < min) min = n;
+        cin>>arr[i];
     }
-    cout << min << " " << max;
+    for (int i = 0; i < 5; i++)
+    {
+        arr_copy[i] = arr[i];
+    }
+    arr_copy[5] = 100;
+    arr_copy[6] = 200;
+    for (int i = 0; i < 7; i++)
+    {
+        cout<<arr_copy[i]<<" ";
+    }
+    cout<<'\n';
+    //delete arr[]
+    delete[] arr;
+    //printing garbage value
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
