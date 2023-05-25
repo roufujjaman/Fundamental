3880ad695684d6cb16d5df66431e6abb08f7a6e3
Roufujjaman <rrhtcode@gmail.com>
2023-05-26 01:48:56 +0600

    26 5 23

---
+++ 23_Practice Static Object.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student
+{
+    public:
+        char name[100];
+        int roll;
+        char section;
+        int math_marks;
+        int class_enrolled;
+
+        Student(char * n, int r, char s, int mm, int ce)
+        {
+            strcpy(name, n);
+            roll = r;
+            section = s;
+            math_marks = mm;
+            class_enrolled = ce;
+        }
+};
+int main()
+{
+    Student student_one = new Student("Roufujjaman", 4010, 'A', 100, 13);
+    Student student_two = new Student("Umme", 3010, 'A', 96, 13);
+    Student student_three = new Student("Ruhul", 1010, 'A', 40, 13);
+
+    if (student_one.math_marks > student_two.math_marks && student_one.math_marks > student_three.math_marks)
+    {
+        cout << student_one.name << ":\t" << student_one.math_marks;
+    }
+    else if (student_two.math_marks > student_one.math_marks && student_two.math_marks > student_three.math_marks)
+    {
+        cout << student_two.name << ":\t" <<student_two.math_marks;
+    }
+    else 
+    {
+        cout << student_three.name << ":\t" << student_three.math_marks;
+    }
+    return 0;
+}
--- sketch.cpp
+++ sketch.cpp
@@ -1,40 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Student
+class Cricketer
 {
     public:
-        char name[100];
-        int roll;
-        char section;
-        int math_marks;
-        int class_enrolled;
-
-        Student(char * n, int r, char s, int mm, int ce)
-        {
-            strcpy(name, n);
-            roll = r;
-            section = s;
-            math_marks = mm;
-            class_enrolled = ce;
-        }
+        int jursey_no;
+        char country[100];
 };
 int main()
 {
-    Student * student_one = new Student("Roufujjaman", 4010, 'A', 100, 13);
-    Student * student_two = new Student("Umme", 3010, 'A', 96, 13);
-    Student * student_three = new Student("Ruhul", 1010, 'A', 40, 13);
+    char country[100] = "Bangladesh";
+    Cricketer * mashrafi = new Cricketer;
+    mashrafi->jursey_no = 10;
+    strcpy(mashrafi->country, country);
 
-    if (student_one->math_marks > student_two->math_marks && student_one->math_marks > student_three->math_marks)
-    {
-        cout << student_one->name << ":\t" << student_one->math_marks;
-    }
-    else if (student_two->math_marks > student_one->math_marks && student_two->math_marks > student_three->math_marks)
-    {
-        cout << student_two->name << ":\t" <<student_two->math_marks;
-    }
-    else 
-    {
-        cout << student_three->name << ":\t" << student_three->math_marks;
-    }
+    delete mashrafi;
+    Cricketer * tamim = new Cricketer;
+    tamim->jursey_no = mashrafi->jursey_no;
+    strcpy(tamim->country, mashrafi->country);
+
+    cout << tamim->jursey_no << endl;
+    cout << tamim->country << endl;
+
     return 0;
 }
