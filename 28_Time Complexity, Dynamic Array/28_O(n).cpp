08bdc787ba6aee8f62f23912a7740033aef9ba58
Roufujjaman <rrhtcode@gmail.com>
2023-06-08 23:43:47 +0600

    8 6 23

---
+++ 28_Time Complexity, Dynamic Array/28_O(n).cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int count;
+    cin >> cout;
+    int arr[count];
+    for( int i = 0; i < count; i++ )
+    {
+        cin >> a[i];
+    }
+    int sum = 0;
+    for ( int i = 0; i < count; i++ )
+    {
+        s += a[i];
+    }
+
+    cout << sum << endl;
+}    
+
+    // 10^5 ------ 0.01s
+    // 10^6 ------ 0.1s
+    // 10^7 ------ 1s (defined)
+    // 10^8 ------ 10s 
+    // 10^9 ------ 100s
